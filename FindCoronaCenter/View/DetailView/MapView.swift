6e96a20fb711bcf8fc553306ea9b226b013c263d
jeonsumin <soomis7@naver.com>
2022-03-03 00:09:14 +0900

    맵 화면 구현

---
+++ FindCoronaCenter/View/DetailView/MapView.swift
@@ -0,0 +1,48 @@
+//
+//  MapView.swift
+//  FindCoronaCenter
+//
+//  Created by Terry on 2022/03/02.
+//
+
+import SwiftUI
+import MapKit
+
+struct AnnotationItem: Identifiable {
+    let id = UUID()
+    var coordinate: CLLocationCoordinate2D
+}
+
+struct MapView: View {
+    var coordination: CLLocationCoordinate2D
+    
+    @State private var region = MKCoordinateRegion()
+    @State private var annotationItems = [AnnotationItem]()
+    
+    var body: some View {
+        Map(coordinateRegion: $region,
+            annotationItems: [AnnotationItem(coordinate: coordination)],
+            annotationContent: {
+            MapMarker(coordinate: $0.coordinate)
+        }
+        )
+            .onAppear {
+                setRegion(coordination)
+                setAnnotaionItems(coordination)
+            }
+    }
+    
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    }
+    private func setAnnotaionItems(_ coordinate: CLLocationCoordinate2D){
+        annotationItems = [AnnotationItem(coordinate: coordinate)]
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        let center0 = Center(id: 0, sido: .경기도, facilityName: "실내빙상장", address: "경기도 뫄뫄시 뫄뫄구", lat: "34.404475", lng: "126.9491998", centerType: .central, phoneNumber: "010-0000-0000")
+        MapView(coordination: CLLocationCoordinate2D(latitude: CLLocationDegrees(center0.lat) ?? .zero, longitude: CLLocationDegrees(center0.lng) ?? .zero))
+    }
+}
