859e966e8491a52ff88093a55f7ffb18a4892815
jeonsumin <soomis7@naver.com>
2022-03-02 22:18:53 +0900

    Initial Commit

---
+++ FindCoronaCenter/FindCoronaCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindCoronaCenterApp.swift
+//  FindCoronaCenter
+//
+//  Created by Terry on 2022/03/02.
+//
+
+import SwiftUI
+
+@main
+struct FindCoronaCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
